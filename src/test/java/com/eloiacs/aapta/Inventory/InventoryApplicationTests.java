a7b017728e86200d16ee1f69a59239f920ec6df0
Tech Rules <66055840+techrules07@users.noreply.github.com>
2024-10-28 12:31:26 +0530

    first commit

---
+++ src/test/java/com/eloiacs/aapta/Inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eloiacs.aapta.Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
