9bae10c2e4c41a8ebdba4e202b97a3539b215184
ArunEloiacs <arun.abp@eloiacs.com>
2024-11-09 18:06:17 +0530

    get purchase done

--- src/main/java/com/eloiacs/aapta/Inventory/RestAPIs/PurchaseController.java
+++ src/main/java/com/eloiacs/aapta/Inventory/RestAPIs/PurchaseController.java
@@ -4,6 +4,8 @@ import com.eloiacs.aapta.Inventory.DBHandler.AuthHandler;
 import com.eloiacs.aapta.Inventory.DBHandler.PurchaseHandler;
 import com.eloiacs.aapta.Inventory.Models.*;
 import com.eloiacs.aapta.Inventory.Responses.BaseResponse;
+import com.eloiacs.aapta.Inventory.Responses.ProductResponse;
+import com.eloiacs.aapta.Inventory.Responses.PurchaseResponse;
 import com.eloiacs.aapta.Inventory.Service.JwtService;
 import com.eloiacs.aapta.Inventory.config.AWSConfig;
 import com.eloiacs.aapta.Inventory.utils.Utils;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/purchase")
@@ -84,6 +87,50 @@ public class PurchaseController {
                 baseResponse.setMessage("PurchaseOrder add Failed");
             }
         } else {
+            baseResponse.setCode(HttpStatus.FORBIDDEN.value());
+            baseResponse.setStatus("Failed");
+            baseResponse.setMessage("Please login again");
+        }
+
+        return baseResponse;
+    }
+
+    @RequestMapping(value = "/getPurchase", method = RequestMethod.POST)
+    public BaseResponse getPurchase(HttpServletRequest httpServletRequest){
+
+        BaseResponse baseResponse = new BaseResponse();
+
+        HashMap<String, Object> claims = jwtService.extractUserInformationFromToken(httpServletRequest.getHeader("Authorization"));
+
+        if (claims != null) {
+
+            String createdBy = claims.get("id").toString();
+            String expireDate = claims.get("exp").toString();
+
+            if (Utils.checkExpired(expireDate)){
+                LoginModel loginModel = authHandler.getUserDetails(createdBy);
+                AuthModel model1 = authHandler.accountDetails(loginModel);
+                if (model1 != null) {
+                    baseResponse.setAccessToken(jwtService.generateJWToken(model1.getEmail(), model1));
+                }
+                else {
+                    baseResponse.setAccessToken("");
+                }
+            }
+
+            List<PurchaseResponse> purchaseResponses = purchaseHandler.getPurchase();
+
+            if (purchaseResponses!=null && !purchaseResponses.isEmpty()){
+                baseResponse.setCode(HttpStatus.OK.value());
+                baseResponse.setStatus("Success");
+                baseResponse.setMessage("Purchase got successfully");
+                baseResponse.setData(purchaseResponses);
+            }else {
+                baseResponse.setCode(HttpStatus.NO_CONTENT.value());
+                baseResponse.setStatus("Failed");
+                baseResponse.setMessage("No purchase found");
+            }
+        }else {
             baseResponse.setCode(HttpStatus.FORBIDDEN.value());
             baseResponse.setStatus("Failed");
             baseResponse.setMessage("Please login again");
