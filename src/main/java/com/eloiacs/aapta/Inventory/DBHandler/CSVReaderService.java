48b0e168dfc752fdd3ec20e905e83280d974eb02
ArunEloiacs <arun.abp@eloiacs.com>
2024-11-10 19:21:37 +0530

    csv upload changes

--- src/main/java/com/eloiacs/aapta/Inventory/DBHandler/CSVReaderService.java
+++ src/main/java/com/eloiacs/aapta/Inventory/DBHandler/CSVReaderService.java
@@ -5,12 +5,16 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Service
@@ -44,7 +48,8 @@ public class CSVReaderService {
 
     public void csvToDbProduct(List<ExcelItem> items){
 
-        String insertProductQuery = "insert into products(productName,statusType,category,subCategory,brand,unit,minPurchaseQuantity,barcodeType,barcodeNo,description,billOfMaterials,freebie,freebieProduct,isActive,createdAt,createdBy) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,current_timestamp(),?)";
+        String insertProductQuery = "insert into products(productName,HSNCode,statusType,category,subCategory,brand,unit,size,minPurchaseQuantity,barcodeType,barcodeNo,description,billOfMaterials,freebie,freebieProduct,isActive,createdAt,createdBy) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,current_timestamp(),?)";
+        String insertProductPriceQuery = "insert into productPrice (productId,category,subCategory,size,mrp,salesPrice,salesPercentage,wholesalePrice,wholesalePercentage) values(?,?,?,?,?,?,?,?,?)";
 
         for(ExcelItem item : items){
 
@@ -55,22 +60,45 @@ public class CSVReaderService {
                 wholesalesPrice = mrp;
             }
 
-            jdbcTemplate.update(insertProductQuery,
-                    item.getItemName(),
-                    1,
-                    item.getCategoryID(),
-                    1,
-                    item.getManufacturer(),
-                    item.getUnit(),
-                    1,
-                    1,
-                    item.getMfgBarcode(),
-                    item.getDescription(),
-                    0,
-                    0,
-                    0,
-                    item.getActiveStatus(),
-                    3);
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            int insertedProduct = jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(insertProductQuery, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, item.getItemName());
+                ps.setString(2,"HSNCode");
+                ps.setInt(3, 1);
+                ps.setInt(4, item.getCategoryID());
+                ps.setInt(5, 1);
+                ps.setInt(6, item.getManufacturer());
+                ps.setInt(7, item.getUnit());
+                ps.setInt(8,1);
+                ps.setInt(9, 1);
+                ps.setInt(10, 1);
+                ps.setString(11, item.getMfgBarcode());
+                ps.setString(12, item.getDescription());
+                ps.setInt(13, 0);
+                ps.setInt(14, 0);
+                ps.setInt(15, 0);
+                ps.setBoolean(16, item.getActiveStatus());
+                ps.setInt(17, 3);
+                return ps;
+            }, keyHolder);
+
+            int insertedProductId = keyHolder.getKey().intValue();
+
+            if (insertedProduct > 0){
+
+                jdbcTemplate.update(insertProductPriceQuery,
+                        insertedProductId,
+                        item.getCategoryID(),
+                        1,
+                        1,
+                        item.getMRP(),
+                        item.getSRate(),
+                        10,
+                        wholesalesPrice,
+                        2);
+            }
         }
     }
 }
